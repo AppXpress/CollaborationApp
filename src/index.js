e3b8a81e2e27f373c0ed897a080a53d8ed722631
Dan Brunwasser <me@danb.link>
2017-07-19 14:08:01 -0400

    Removed old IssueManagerReact code

--- src/index.js
+++ src/index.js
@@ -1,33 +1,20 @@
-import { Navigation } from 'react-native-navigation';
+import {
+    Navigation
+} from 'react-native-navigation';
 
 import Login from './pages/Login';
-import IssueList from './pages/IssueList';
-import FilterIssues from './pages/FilterIssues';
-import IssueDetails from './pages/IssueDetails';
-import CreateIssue from './pages/CreateIssue';
-import CreateMessage from './pages/CreateMessage';
-import CameraScreen from './pages/CameraScreen';
-import CameraDisplay from './pages/CameraDisplay';
 import Settings from './pages/Settings';
 
 /**
-*	Entry-point for all devices. Registers all necesarry pages
-*	to the navigation library.
-*/
+ *	Entry-point for all devices. Registers all necesarry pages
+ *	to the navigation library.
+ */
 Navigation.registerComponent('Login', () => Login);
-Navigation.registerComponent('IssueList', () => IssueList);
-Navigation.registerComponent('FilterIssues', () => FilterIssues)
-Navigation.registerComponent('IssueDetails', () => IssueDetails);
-Navigation.registerComponent('CreateIssue', () => CreateIssue);
-Navigation.registerComponent(
-    'ImageDisplay', () => require('./pages/ImageDisplay').default
-);
-Navigation.registerComponent('CreateMessage', () => CreateMessage);
-Navigation.registerComponent('CameraScreen', () => CameraScreen);
-Navigation.registerComponent('CameraDisplay', () => CameraDisplay);
 Navigation.registerComponent('Settings', () => Settings);
 
 
 Navigation.startSingleScreenApp({
-    screen: { screen: 'Login' }
+    screen: {
+        screen: 'Login'
+    }
 });
