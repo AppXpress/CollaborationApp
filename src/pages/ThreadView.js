b087f1c2ef0133a66259e88a8193715857a6d484
ptz3233 <ptz3233@gmail.com>
2017-07-26 14:52:07 -0400

    added support for replys again

--- src/pages/ThreadView.js
+++ src/pages/ThreadView.js
@@ -75,8 +75,24 @@ export default class View extends Component {
         AppX.query('&comment', `Parent.rootId = ${this.props.uid}` + ' ORDER BY createTimestamp ASC').then(result => {
             this.setState({
                 comments: result.data.result || []
+            }, ()=>{
+            this.state.comments.forEach((comment)=> {
+                if(comment.ReplyTo){
+
+                    var uid = comment.ReplyTo.rootId;
+                    var replyed = this.state.comments.find(function(comment){
+                        return comment.uid == uid;
+                    })
+                    comment.replyed = {};
+                    comment.replyed.Body = replyed.Body;
+                    comment.replyed.Author = replyed.Author;
+                    console.log(comment)
+                }
+                this.setState({});
             });
         });
+
+    });        
 
     }
 
@@ -113,12 +129,23 @@ export default class View extends Component {
         });
     }
 
-    renderComment = item => {
+   renderComment = item => {
+        
+        
         return (
             <ListItem
                 key={item.uid}
                 onPress={() => this.viewComment(item)}
             >
+            {item.replyed && 
+                <ListItem>
+                <ComplexText
+                    main='Replying To'
+                    secondary={item.replyed.Body}
+                    tertiary={item.replyed.Author}
+                 />   
+                 </ListItem>
+            }    
                 <ComplexText
                     main={item.Body}
                     secondary={item.Date + ' at ' + helpers.formatTime(item.Time)}
